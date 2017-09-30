2018cb94f6936d7c5e58445d5acc16f91c0b4edf
Ramesh Mahadevan <ramahadevan@ebay.com>
2017-09-29 23:45:51 -0700

    removed unwanted

--- fixtures/users-list/test/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var assert = require('assert');
-
-var usersHandler = require('../').handler;
-usersHandler.call(this, undefined, undefined, function (err, result){
-  assert.ok(!err);
-  assert.ok(result);
-});
